4ac3017360789295cb57d18bb9c5ba1269de419e
w33h <academyynetworking@gmail.com>
2022-04-12 23:12:41 +0800

    feat(core): add core to main

--- main.go
+++ main.go
@@ -2,7 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/w33h/Hexagonal-Architecture-Go/api"
+	"github.com/w33h/Hexagonal-Architecture-Go/app/modules"
+	"github.com/w33h/Hexagonal-Architecture-Go/config"
+	"github.com/w33h/Hexagonal-Architecture-Go/util"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 
@@ -10,7 +15,19 @@ import (
 )
 
 func main() {
+	config := config.GetConfig()
+
+	dbCon := util.NewConnectionDatabase(config)
+	defer dbCon.CloseConnection()
+
+	controllers := modules.RegisterModules(dbCon)
+
 	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "success")
+	})
+	api.RegistrationPath(e, controllers)
+
 	go func() {
 		adddress := fmt.Sprintf(":%d", 4001)
 		if err := e.Start(adddress); err != nil {
