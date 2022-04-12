fc216e4bfa9e325f870af2aa356246bdc02e6e84
w33h <academyynetworking@gmail.com>
2022-04-12 23:08:56 +0800

    feat(module): add feat module for DI

---
+++ app/modules/modules.go
@@ -0,0 +1,22 @@
+package modules
+
+import (
+	"github.com/w33h/Hexagonal-Architecture-Go/api"
+	userV1Controller "github.com/w33h/Hexagonal-Architecture-Go/api/v1/user"
+	userService "github.com/w33h/Hexagonal-Architecture-Go/service/user"
+	userRepository "github.com/w33h/Hexagonal-Architecture-Go/repository"
+	"github.com/w33h/Hexagonal-Architecture-Go/util"
+)
+
+func RegisterModules(dbCon *util.DatabaseConnection) api.Controller {
+	userPermitRepository := userRepository.RepositoryFactory(dbCon)
+	userPermitService := userService.NewService(userPermitRepository)
+
+	userV1PermitController := userV1Controller.NewController(userPermitService)
+
+	controller := api.Controller{
+		UserV1Controller: userV1PermitController,
+	}
+
+	return controller
+}
