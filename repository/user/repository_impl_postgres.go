291abfd0f72112cd9fc864186298ccb68ba9bfc9
w33h <academyynetworking@gmail.com>
2022-04-13 23:00:10 +0800

    delete: unused code

--- repository/user/repository_impl_postgres.go
+++ repository/user/repository_impl_postgres.go
@@ -1,7 +1,7 @@
-package user
+package repository
 
 import (
-	"github.com/w33h/Hexagonal-Architecture-Go/business/user"
+	domain "github.com/w33h/Hexagonal-Architecture-Go/business/user"
 	"gorm.io/gorm"
 )
 
@@ -9,44 +9,13 @@ type postgresRepository struct {
 	db *gorm.DB
 }
 
-func NewPostgresRepository(db *gorm.DB) user.Repository {
+func NewPostgresRepository(db *gorm.DB) UserRepository {
 	return &postgresRepository{
 		db: db,
 	}
 }
 
-//func (repo *postgresRepository) Save(user user.Users) (user.Users, error) {
-//	err := repo.db.Create(&user)
-//	if err != nil {
-//		return user, err.Error
-//	}
-//
-//	return user, nil
-//}
-//
-//func (repo *postgresRepository) Update(user user.Users) (user.Users, error) {
-//	user.UpdatedAt = time.Now()
-//
-//	err := repo.db.Save(&user)
-//	if err != nil {
-//		return user, err.Error
-//	}
-//
-//	return user, nil
-//}
-//
-//func (repo *postgresRepository) Delete(Id int) (err error) {
-//	var user user.Users
-//
-//	err = repo.db.Where("Id = ?", Id).Delete(&user).Error
-//	if err != nil {
-//		return err
-//	}
-//
-//	return nil
-//}
-
-func (repo *postgresRepository) FindById(Id int) (user *user.Users, err error) {
+func (repo *postgresRepository) FindById(Id int) (user *domain.Users, err error) {
 	err = repo.db.Where("Id = ?", Id).First(&user).Error
 	if err != nil {
 		return nil, err
@@ -54,12 +23,3 @@ func (repo *postgresRepository) FindById(Id int) (user *user.Users, err error) {
 
 	return user, nil
 }
-
-//func (repo *postgresRepository) FindAll() (users []user.Users, err error) {
-//	err = repo.db.Find(&users).Error
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	return users, nil
-//}
